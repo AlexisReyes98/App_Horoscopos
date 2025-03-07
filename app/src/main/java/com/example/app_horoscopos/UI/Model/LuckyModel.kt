4b9537ec70b1a9cb46438ebb3010dc722cf774fc
AlexisReyes98 <giovanny29.garv@gmail.com>
2025-03-06 22:55:21 -0600

    Commit: Clases de la semana 26/02/25 al 06/03/25 luck fragment terminado.

---
+++ app/src/main/java/com/example/app_horoscopos/UI/Model/LuckyModel.kt
@@ -0,0 +1,9 @@
+package com.example.app_horoscopos.UI.Model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class LuckyModel (
+    @DrawableRes val image:Int,
+    @StringRes val text:Int
+)
